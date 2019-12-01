f39e776d949fc437e8510524cd73bfd878c7f661
Alex Engelhart <asengelhart@yahoo.com>
2019-12-01 00:23:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,6 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   rail_hash = base_hash
+  rail_hash[:railroads][:pieces] = nil
 end
 
 def monopoly_with_third_tier
